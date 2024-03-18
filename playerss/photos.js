201afae0c9b8d620e7776bc78ef9deafbf757d34
daryljimenea <daryl@montalvokd.com>
2024-03-19 01:25:24 +0800

    Updates

---
+++ playerss/photos.js
@@ -0,0 +1,256 @@
+WPGgallery = new Object();
+WPGgallery.name = "Adobe%20Web%20Photo%20Gallery";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "7/4/2013"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "01playermain.jpg";
+gPhotos[0].ImageWidth = 590;
+gPhotos[0].ImageHeight = 409;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 52;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "02PlayerWagerParlay.jpg";
+gPhotos[1].ImageWidth = 590;
+gPhotos[1].ImageHeight = 409;
+gPhotos[1].ThumbWidth = 75;
+gPhotos[1].ThumbHeight = 52;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "03PlayerWagerStraight.jpg";
+gPhotos[2].ImageWidth = 590;
+gPhotos[2].ImageHeight = 409;
+gPhotos[2].ThumbWidth = 75;
+gPhotos[2].ThumbHeight = 52;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "04PlayerWagerTeaser.jpg";
+gPhotos[3].ImageWidth = 590;
+gPhotos[3].ImageHeight = 409;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 52;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "05PlayerWagerPleaser.jpg";
+gPhotos[4].ImageWidth = 590;
+gPhotos[4].ImageHeight = 409;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 52;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "06PlayerWagerActionPoints.jpg";
+gPhotos[5].ImageWidth = 590;
+gPhotos[5].ImageHeight = 409;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 52;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "07PlayerPlaceWagerStatus.jpg";
+gPhotos[6].ImageWidth = 590;
+gPhotos[6].ImageHeight = 343;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 43;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "08PlayerWMReview.jpg";
+gPhotos[7].ImageWidth = 590;
+gPhotos[7].ImageHeight = 410;
+gPhotos[7].ThumbWidth = 75;
+gPhotos[7].ThumbHeight = 52;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "09PlayerWMOpen.jpg";
+gPhotos[8].ImageWidth = 590;
+gPhotos[8].ImageHeight = 410;
+gPhotos[8].ThumbWidth = 75;
+gPhotos[8].ThumbHeight = 52;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "10PlayerWMInProgress.jpg";
+gPhotos[9].ImageWidth = 590;
+gPhotos[9].ImageHeight = 410;
+gPhotos[9].ThumbWidth = 75;
+gPhotos[9].ThumbHeight = 52;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "11PlayerWMInClosed.jpg";
+gPhotos[10].ImageWidth = 590;
+gPhotos[10].ImageHeight = 410;
+gPhotos[10].ThumbWidth = 75;
+gPhotos[10].ThumbHeight = 52;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "12PlayerRecordWagerStraight.jpg";
+gPhotos[11].ImageWidth = 590;
+gPhotos[11].ImageHeight = 410;
+gPhotos[11].ThumbWidth = 75;
+gPhotos[11].ThumbHeight = 52;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "13PlayerAddBookInfo.jpg";
+gPhotos[12].ImageWidth = 590;
+gPhotos[12].ImageHeight = 425;
+gPhotos[12].ThumbWidth = 75;
+gPhotos[12].ThumbHeight = 54;
+gPhotos[12].meta = new Object();
+
+gPhotos[13] = new Object();
+gPhotos[13].filename = "14PlayerAddBookType.jpg";
+gPhotos[13].ImageWidth = 590;
+gPhotos[13].ImageHeight = 425;
+gPhotos[13].ThumbWidth = 75;
+gPhotos[13].ThumbHeight = 54;
+gPhotos[13].meta = new Object();
+
+gPhotos[14] = new Object();
+gPhotos[14].filename = "15PlayerAddBookInvite.jpg";
+gPhotos[14].ImageWidth = 590;
+gPhotos[14].ImageHeight = 446;
+gPhotos[14].ThumbWidth = 75;
+gPhotos[14].ThumbHeight = 56;
+gPhotos[14].meta = new Object();
+
+gPhotos[15] = new Object();
+gPhotos[15].filename = "16PlayerAddBookPassword.jpg";
+gPhotos[15].ImageWidth = 590;
+gPhotos[15].ImageHeight = 425;
+gPhotos[15].ThumbWidth = 75;
+gPhotos[15].ThumbHeight = 54;
+gPhotos[15].meta = new Object();
+
+gPhotos[16] = new Object();
+gPhotos[16].filename = "17PlayerAddBookCongrats.jpg";
+gPhotos[16].ImageWidth = 590;
+gPhotos[16].ImageHeight = 425;
+gPhotos[16].ThumbWidth = 75;
+gPhotos[16].ThumbHeight = 54;
+gPhotos[16].meta = new Object();
+
+gPhotos[17] = new Object();
+gPhotos[17].filename = "18PlayerBookMaintenence.jpg";
+gPhotos[17].ImageWidth = 590;
+gPhotos[17].ImageHeight = 410;
+gPhotos[17].ThumbWidth = 75;
+gPhotos[17].ThumbHeight = 52;
+gPhotos[17].meta = new Object();
+
+gPhotos[18] = new Object();
+gPhotos[18].filename = "19PlayerPolicyLimits.jpg";
+gPhotos[18].ImageWidth = 590;
+gPhotos[18].ImageHeight = 407;
+gPhotos[18].ThumbWidth = 75;
+gPhotos[18].ThumbHeight = 51;
+gPhotos[18].meta = new Object();
+
+gPhotos[19] = new Object();
+gPhotos[19].filename = "20PlayerPolicyBuyPoints.jpg";
+gPhotos[19].ImageWidth = 590;
+gPhotos[19].ImageHeight = 409;
+gPhotos[19].ThumbWidth = 75;
+gPhotos[19].ThumbHeight = 52;
+gPhotos[19].meta = new Object();
+
+gPhotos[20] = new Object();
+gPhotos[20].filename = "21PlayerPolicyParlayRules.jpg";
+gPhotos[20].ImageWidth = 590;
+gPhotos[20].ImageHeight = 409;
+gPhotos[20].ThumbWidth = 75;
+gPhotos[20].ThumbHeight = 52;
+gPhotos[20].meta = new Object();
+
+gPhotos[21] = new Object();
+gPhotos[21].filename = "22PlayerPolicyParlayOdds.jpg";
+gPhotos[21].ImageWidth = 590;
+gPhotos[21].ImageHeight = 409;
+gPhotos[21].ThumbWidth = 75;
+gPhotos[21].ThumbHeight = 52;
+gPhotos[21].meta = new Object();
+
+gPhotos[22] = new Object();
+gPhotos[22].filename = "23PlayerPolicyTeaserRules.jpg";
+gPhotos[22].ImageWidth = 590;
+gPhotos[22].ImageHeight = 409;
+gPhotos[22].ThumbWidth = 75;
+gPhotos[22].ThumbHeight = 52;
+gPhotos[22].meta = new Object();
+
+gPhotos[23] = new Object();
+gPhotos[23].filename = "24PlayerPolicyTeaserOdds.jpg";
+gPhotos[23].ImageWidth = 590;
+gPhotos[23].ImageHeight = 409;
+gPhotos[23].ThumbWidth = 75;
+gPhotos[23].ThumbHeight = 52;
+gPhotos[23].meta = new Object();
+
+gPhotos[24] = new Object();
+gPhotos[24].filename = "25PlayerPolicyActionPoints.jpg";
+gPhotos[24].ImageWidth = 590;
+gPhotos[24].ImageHeight = 409;
+gPhotos[24].ThumbWidth = 75;
+gPhotos[24].ThumbHeight = 52;
+gPhotos[24].meta = new Object();
+
+gPhotos[25] = new Object();
+gPhotos[25].filename = "26PlayerNotification.jpg";
+gPhotos[25].ImageWidth = 590;
+gPhotos[25].ImageHeight = 430;
+gPhotos[25].ThumbWidth = 75;
+gPhotos[25].ThumbHeight = 54;
+gPhotos[25].meta = new Object();
+
+gPhotos[26] = new Object();
+gPhotos[26].filename = "27PlayerBalances.jpg";
+gPhotos[26].ImageWidth = 590;
+gPhotos[26].ImageHeight = 410;
+gPhotos[26].ThumbWidth = 75;
+gPhotos[26].ThumbHeight = 52;
+gPhotos[26].meta = new Object();
+
+gPhotos[27] = new Object();
+gPhotos[27].filename = "28PlayerBalancesTotal.jpg";
+gPhotos[27].ImageWidth = 590;
+gPhotos[27].ImageHeight = 410;
+gPhotos[27].ThumbWidth = 75;
+gPhotos[27].ThumbHeight = 52;
+gPhotos[27].meta = new Object();
+
+gPhotos[28] = new Object();
+gPhotos[28].filename = "29PlayerBoxScores.jpg";
+gPhotos[28].ImageWidth = 590;
+gPhotos[28].ImageHeight = 410;
+gPhotos[28].ThumbWidth = 75;
+gPhotos[28].ThumbHeight = 52;
+gPhotos[28].meta = new Object();
+
+gPhotos[29] = new Object();
+gPhotos[29].filename = "30PlayerGamesInProgress.jpg";
+gPhotos[29].ImageWidth = 590;
+gPhotos[29].ImageHeight = 409;
+gPhotos[29].ThumbWidth = 75;
+gPhotos[29].ThumbHeight = 52;
+gPhotos[29].meta = new Object();
+
